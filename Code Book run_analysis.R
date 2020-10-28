c5d8e0e5e4d631d2dfa7fc5f2ab22e3cd223b0fe
dfjohnson90 <72209022+dfjohnson90@users.noreply.github.com>
2020-10-28 09:20:49 -0700

    Create Code Book run_analysis.R

---
+++ Code Book run_analysis.R
@@ -0,0 +1,35 @@
+tidyData Code Book
+
+Cleaning Data Project Week 4 CourseRA
+
+subject: numeric representation of the subject performing the activity. Numbered 1-30
+
+activity: which activity is being performed by the subject. There are six different categories as follows:
+	•	walking
+	•	walking upstairs
+	•	walking downstairs
+	•	sitting
+	•	standing
+	•	laying
+
+The values in the rest of the columns are the computed mean values of the data collect on that subject performing that action. For example the third column of the first row is the computed mean value from every data point (TimeBodyAccMeanX) collected on subject 1, while standing.
+
+Abbreviations in the variable names are described below:
+
+Std: Standard Deviation
+
+Mean: Mean value
+
+Acc - Acceleration
+
+Gyro - Angular velocity
+
+Mag - Magnitude
+
+Freq - Frequency
+
+X - X-axis
+
+Y - Y-axis
+
+Z - Z-axis
